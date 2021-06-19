41260d4dae5eb7812473e60320eaea6e12d10238
D0TheMath <gman7615@gmail.com>
2021-06-19 18:56:18 -0400

    Added Twitter search function
    This will be used to find tweets with a particular hashtag, keyword, or which have been tweeted by a particular author.

--- functions.py
+++ functions.py
@@ -11,3 +11,22 @@ def tweet(text):
 
     # posting tweet
     api.update_status(text)
+
+# returns 2d array with tweet id in the first row (as an integer), and the tweet text in the second row (as a string)
+def search(text):
+    # authentification
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # getting tweepy api object
+    api = tweepy.API(auth)
+
+    # limiting to 100 items, since more seems overkill
+    max_results = 100
+    results = list()
+
+    # Using twitter search function to search for inputted text
+    for i,tweet in enumerate(tweepy.Cursor(api.search, q=text).items(max_results)):
+        results.append([tweet.id, tweet.text])
+
+    return results

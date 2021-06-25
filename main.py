d4ddf23df797c7860dfbc82a89dcdbd4cbc35120
D0TheMath <gman7615@gmail.com>
2021-06-25 12:50:15 -0400

    Added retweeting of mentions functionality
    This version is untested, so report bugs if you test it yourself & it doesn't work.

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ while True:
     for hashtag in hashtags:
         # retweets instances of that hashtag found in past 7 days
         retweetHashtag(hashtag)
+
+    # loops through all the mentions of the bot on the timeline
+    for mention in mentions():
+        # attempts to retweet mention
+        try:
+            # but only if it isn't in reply to anything
+            if mention.in_reply_to_status_id is None:
+                retweet(mention.id)
+        # If an error happens (usually when the tweet has already been retweeted)
+        # then it does nothing
+        except:
+            pass
     # waits 5 seconds
     # (may want to make this longer, perhapse tweets once every 10 minutes instead
     # or even once every day)
